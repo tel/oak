b4fad190273aa99aac4a8b9370f9a8ba3a143cba
Joseph Abrahamson <me@jspha.com>
2016-04-20 22:16:40 -0400

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oak "0.1.2-SNAPSHOT"
+(defproject oak "0.1.2"
   :description "Drop-dead simple, super-compositional UI components"
   :url "http://github.com/tel/cljs-oak"
   :license {:name "Eclipse Public License"
