5e749c5261f486cac32f7b9ae6931a25e533201e
Joseph Abrahamson <me@jspha.com>
2016-05-26 18:04:00 -0400

    bump to 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oak "0.1.3-SNAPSHOT"
+(defproject oak "0.2.0-SNAPSHOT"
   :description "Drop-dead simple, super-compositional UI components"
   :url "http://github.com/tel/cljs-oak"
   :license {:name "Eclipse Public License"
